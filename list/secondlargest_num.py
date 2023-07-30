68ac1e71fcb37e82797831ee851fad2a562d18fb
deep <deepaidasani1435@gmail.com>
2023-07-30 14:45:06 +0530

    ok

--- list/secondlargest_num.py
+++ list/secondlargest_num.py
@@ -1,1 +1,19 @@
-print("hii")
+class Solution:
+
+	def print2largest(self,arr, n):
+		lar=arr[0]
+		slar=None
+	
+		for x in arr[1:]:
+		    if x>lar:
+		        slar=lar
+		        lar=x
+		    elif x!=lar:
+		        if slar==None or slar<x:
+		            slar=x
+		            
+        if slar==None:
+            return -1           
+		return slar          
+
+
